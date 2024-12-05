cf388d6ddb28a9682dbb1c05f359b370e14b9562
user-9902 <xy_frontend@163.com>
2024-12-05 15:16:52 +0800

    feat: block  添加 csdn 顶部广告block

--- block/dist.js
+++ block/dist.js
@@ -1,6 +1,7 @@
 ;(function () {
   'use strict'
   const url = location.href
+  const host = location.host
   // 必应搜索
   if (url.startsWith('https://cn.bing.com/search')) {
     // 删除广告
@@ -9,11 +10,26 @@
       if (/b_ad/.test(el.className)) {
         el.style.display = 'none'
       }
+      let i = el.querySelector('.b_lineclamp2')
+
+      setTimeout(() => {
+        if (
+          i &&
+          getComputedStyle(i, ':before').content.startsWith(
+            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAICAYAAADjoT9j',
+          )
+        ) {
+          el.style.display = 'none'
+        }
+      }, 100)
     }
   }
 
   // csdn
-  else if (url.startsWith('https://blog.csdn.net/')) {
-    // todo 删除广告
+  else if (host.endsWith('.csdn.net')) {
+    const el = document.getElementById('toolbarBox')
+    for (let i = 0; i < el.chilren.length - 1; i++) {
+      el.children[i].display = 'none'
+    }
   }
 })()
